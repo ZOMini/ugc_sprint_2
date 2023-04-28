10d459d8b5578102336b699b4965b88e3169e717
ZOMini <zarikacr@gmail.com>
2023-04-28 08:53:25 +0300

    v2.4

--- ugc_api/core/config.py
+++ ugc_api/core/config.py
@@ -2,16 +2,16 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    kafka_url: str = Field('localhost:9092')
-    kafka_client: str = Field('kafka-movies-api')
-    topic_list: list = Field(['views'])
-    project_name: str = Field('ugc_movies_api')
-    mongo_db: str = Field(...)
-    mongo_url: str = Field(...)
-    sentry_dns: str = Field(...)
-    logstash_host: str = Field(...)
-    logstash_port: int = Field(...)
-    log_level: str = Field('INFO')
+    kafka_url: str = Field('localhost:9092')  # noqa: call-arg
+    kafka_client: str = Field('kafka-movies-api')  # noqa: call-arg
+    topic_list: list = Field(['views'])  # noqa: call-arg
+    project_name: str = Field('ugc_movies_api')  # noqa: call-arg
+    mongo_db: str = Field(...)  # noqa: call-arg
+    mongo_url: str = Field(...)  # noqa: call-arg
+    sentry_dns: str = Field(...)  # noqa: call-arg
+    logstash_host: str = Field(...)  # noqa: call-arg
+    logstash_port: int = Field(...)  # noqa: call-arg
+    log_level: str = Field('INFO')  # noqa: call-arg
 
     class Config:
         env_file = '../.env'
